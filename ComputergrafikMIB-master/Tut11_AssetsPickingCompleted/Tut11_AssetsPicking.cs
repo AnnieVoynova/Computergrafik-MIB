58423e194a4e07073346a77c1136c2de05cf8713
AnnieVoynova <64129898+AnnieVoynova@users.noreply.github.com>
2020-06-15 11:12:21 +0200

    Add files via upload

---
+++ ComputergrafikMIB-master/Tut11_AssetsPickingCompleted/Tut11_AssetsPicking.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Fusee.Base.Common;
+using Fusee.Base.Core;
+using Fusee.Engine.Common;
+using Fusee.Engine.Core;
+using Fusee.Math.Core;
+using Fusee.Serialization;
+using Fusee.Xene;
+using static System.Math;
+using static Fusee.Engine.Core.Input;
+using static Fusee.Engine.Core.Time;
+
+
+namespace FuseeApp
+{
+    [FuseeApplication(Name = "Tut11_AssetsPicking", Description = "Yet another FUSEE App.")]
+    public class Tut11_AssetsPicking : RenderCanvas
+    {
+        private SceneContainer _scene;
+        private SceneRendererForward _sceneRenderer;
+        private ScenePicker _scenePicker;
+        private TransformComponent _baseTransform;
+        private TransformComponent _rightRearTransform;
+        private PickResult _currentPick;
+        private float4 _oldColor;
+
+
+        SceneContainer CreateScene()
+        {
+            // Initialize transform components that need to be changed inside "RenderAFrame"
+            _baseTransform = new TransformComponent
+            {
+                Rotation = new float3(0, 0, 0),
+                Scale = new float3(1, 1, 1),
+                Translation = new float3(0, 0, 0)
+            };
+
+            // Setup the scene graph
+            return new SceneContainer
+            {
+                Children = new List<SceneNodeContainer>
+                {
+                    new SceneNodeContainer
+                    {
+                        Components = new List<SceneComponentContainer>
+                        {
+                            // TRANSFROM COMPONENT
+                            _baseTransform,
+
+                            // SHADER EFFECT COMPONENT
+                            new ShaderEffectComponent
+                            {
+                                Effect = SimpleMeshes.MakeShaderEffect(new float3(0.7f, 0.7f, 0.7f), new float3(1, 1, 1), 5)
+                            },
+
+                            // MESH COMPONENT
+                            SimpleMeshes.CreateCuboid(new float3(10, 10, 10))
+                        }
+                    },
+                }
+            };
+        }
+
+        // Init is called on startup. 
+        public override void Init()
+        {
+            // Set the clear color for the backbuffer to white (100% intensity in all color channels R, G, B, A).
+            RC.ClearColor = new float4(0.8f, 0.9f, 0.7f, 1);
+
+            _scene = AssetStorage.Get<SceneContainer>("CubeCar.fus");
+
+            _rightRearTransform = _scene.Children.FindNodes(node => node.Name == "RightRearWheel")?.FirstOrDefault()?.GetTransform();
+
+            // Create a scene renderer holding the scene above
+            _sceneRenderer = new SceneRendererForward(_scene);
+            _scenePicker = new ScenePicker(_scene);
+        }
+
+        // RenderAFrame is called once a frame
+        public override void RenderAFrame()
+        {
+            SetProjectionAndViewport();
+
+            _rightRearTransform.Rotation = new float3(M.MinAngle(TimeSinceStart), 0, 0);
+
+            // Clear the backbuffer
+            RC.Clear(ClearFlags.Color | ClearFlags.Depth);
+
+            // Setup the camera 
+            RC.View = float4x4.CreateTranslation(0, 0, 40) * float4x4.CreateRotationX(-(float) Atan(15.0 / 40.0));
+
+            if (Mouse.LeftButton)
+            {
+                float2 pickPosClip = Mouse.Position * new float2(2.0f / Width, -2.0f / Height) + new float2(-1, 1);
+                _scenePicker.View = RC.View;
+                _scenePicker.Projection = RC.Projection;
+
+                List<PickResult> pickResults = _scenePicker.Pick(pickPosClip).ToList();
+                PickResult newPick = null;
+                if (pickResults.Count > 0)
+                {
+                    pickResults.Sort((a, b) => Sign(a.ClipPos.z - b.ClipPos.z));
+                    newPick = pickResults[0];
+                }
+
+                if (newPick?.Node != _currentPick?.Node)
+                {
+                    if (_currentPick != null)
+                    {
+                        ShaderEffectComponent shaderEffectComponent = _currentPick.Node.GetComponent<ShaderEffectComponent>();
+                        shaderEffectComponent.Effect.SetEffectParam("DiffuseColor", _oldColor);
+                    }
+                    if (newPick != null)
+                    {
+                        ShaderEffectComponent shaderEffectComponent = newPick.Node.GetComponent<ShaderEffectComponent>();
+                        _oldColor = (float4)shaderEffectComponent.Effect.GetEffectParam("DiffuseColor");
+                        shaderEffectComponent.Effect.SetEffectParam("DiffuseColor", new float4(1, 0.4f, 0.4f, 1));
+                    }
+                    _currentPick = newPick;
+                }
+            }
+
+            // Render the scene on the current render context
+            _sceneRenderer.Render(RC);
+
+            // Swap buffers: Show the contents of the backbuffer (containing the currently rendered farame) on the front buffer.
+            Present();
+        }
+
+
+        public void SetProjectionAndViewport()
+        {
+            // Set the rendering area to the entire window size
+            RC.Viewport(0, 0, Width, Height);
+
+            // Create a new projection matrix generating undistorted images on the new aspect ratio.
+            var aspectRatio = Width / (float)Height;
+
+            // 0.25*PI Rad -> 45° Opening angle along the vertical direction. Horizontal opening angle is calculated based on the aspect ratio
+            // Front clipping happens at 1 (Objects nearer than 1 world unit get clipped)
+            // Back clipping happens at 2000 (Anything further away from the camera than 2000 world units gets clipped, polygons will be cut)
+            var projection = float4x4.CreatePerspectiveFieldOfView(M.PiOver4, aspectRatio, 1, 20000);
+            RC.Projection = projection;
+        }                
+
+    }
+}
